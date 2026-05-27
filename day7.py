549e1dcfe97b58b9a2a13f10010a9d6d12655a47
DhruvMarwal <dhruvmarwal304@gmail.com>
2026-05-27 12:29:20 +0530

    DeleteNode

--- day7.py
+++ day7.py
@@ -89,17 +89,17 @@ class BST:
         if root is None:
             return
         
-        self.preOrder(root.left)
-        self.preOrder(root.right)
+        self.postOrder(root.left)
+        self.postOrder(root.right)
         print(root.data, end = " ")
 
     def InOrder(self, root):
         if root is None:
             return
         
-        self.preOrder(root.left)
+        self.InOrder(root.left)
         print(root.data, end = " ")
-        self.preOrder(root.right)
+        self.InOrder(root.right)
 
     def height(self, root):
         if root is None:
@@ -125,7 +125,7 @@ class BST:
 
         return l_count + r_count + 1
     
-    def binarysearch(self, root, key):              # Mine
+    def binarySearchCheck(self, root, key):              # Mine
         if root is None:
             return "No"
         
@@ -138,12 +138,25 @@ class BST:
         else:
             return self.binarysearch(root.right, key)
         
-    def smallestValue(self, root):
+    def binarysearch(self, root, key):              # Mine
+        if root is None:
+            return "No"
+        
+        if root.data == key:
+            return root
+        
+        if key < root.data:
+            return self.binarysearch(root.left, key)
+        
+        else:
+            return self.binarysearch(root.right, key)
+        
+    def smallestValue(self, root):      # returns adddress of smallest root
         if root is None:
             return None
 
         if root.left is None:
-            return root.data
+            return root
 
         return self.smallestValue(root.left)
     
@@ -156,7 +169,7 @@ class BST:
 
     #     return root.data
     
-    def largestValue(self, root):
+    def largestValue(self, root):       # returns valus of largest root
         if root is None:
             return None
         
@@ -164,43 +177,116 @@ class BST:
             return root.data
         
         return self.largestValue(root.right)
+    
+    # def delete0child(self, root, key):        # Mine
+    #     root = self.binarysearch(root, key)
+    #     root.data = None
+
+    # def delete1child(self, root, key):
+    #     root = self.binarysearch(root, key)
+
+    #     if root.left is None:
+    #         root.right = None
+
+    #     if root.right is None:        
+    #         root.left = None
+
+    # def deletemid(self, root, key):
+    #     root = self.binarysearch(root,key)
+    #     root.data = self.smallestValue(root.right)
+
+    def deleteNode(self, root, key):
+
+        if root is None:
+            return None
+        
+        if key < root.data:
+            root.left = self.deleteNode(root.left, key)
+
+        elif key > root.data:
+            root.right = self.deleteNode(root.right, key)
+
+        else:
+            # 0 child
+            if root.left is None and root.right is None:
+                return None
+            
+            # 1 child
+            if root.left is None:
+                return root.right
+            
+            if root.right is None:
+                return root.left
+            
+            # 2 child
+            else:
+                iso = self.smallestValue(root.right)
+                root.data = iso.data
+                self.deleteNode(root.right, iso.data)
+
+        return root
+
 
 bst = BST()
 root = None
-root = bst.insert(root, 10)
-root = bst.insert(root, 20)
-root = bst.insert(root, 30)
-root = bst.insert(root,  8)
-root = bst.insert(root,  5)
-root = bst.insert(root,  2)
-root = bst.insert(root, 9)
+arr = [10, 20, 18, 30, 8, 5, 2, 9]
+for i in arr:
+    root = bst.insert(root, i)
 
-#         10
-#        /  \
-#       8    20
-#      / \     \
-#     5   9     30
+# root = bst.insert(root, 10)
+# root = bst.insert(root, 20)
+# root = bst.insert(root, 18)
+# root = bst.insert(root, 30)
+# root = bst.insert(root,  8)
+# root = bst.insert(root,  5)
+# root = bst.insert(root,  2)
+# root = bst.insert(root,  9)
+
+#          10
+#        /    \
+#       8      20
+#      / \    /  \
+#     5   9 18    30
 #    /
 #   2
 
 print("Root: ", root.data)      # Root:  10
 
 print("Inorder: ")              #Inorder: 
-bst.InOrder(root)               # 8 5 2 9 10 20 30  
+bst.InOrder(root)               # 2 5 8 9 10 18 20 30
+print()
 
 print("\nPreorder: ")           # Preorder: 
-bst.preOrder(root)              # 10 8 5 2 9 20 30 
+bst.preOrder(root)              # 10 8 5 2 9 20 18 30
+print()
 
 print("\nPostorder: ")          # Postorder: 
-bst.postOrder(root)             # 8 5 2 9 20 30 10
+bst.postOrder(root)             # 2 5 9 8 18 30 20 10 
+print()
 
 print("\nHeight of Tree: ", bst.height(root))       # Height of Tree:  4
 
-print("Sum of all nodes: ", bst.sumOfAllNodes(root))    # Sum of all nodes:  84
+print("Sum of all nodes: ", bst.sumOfAllNodes(root))    # Sum of all nodes: 102
 
-print("Count of nodes: ", bst.countnumberOfNodes(root))     # Count of nodes:  7
+print("Count of nodes: ", bst.countnumberOfNodes(root))     # Count of nodes:  8
 
-print("Is 10 there?: ", bst.binarysearch(root, 10))    # Is 10 there?:  Yes
+print("Is 10 there?: ", bst.binarySearchCheck(root, 10))    # Is 10 there?:  Yes
+print("Is 10 there?: ", bst.binarysearch(root, 10))    # Is 10 there?:  <__main__.Node object at 0x000001DEDC0B01A0>
 
-print("Smallest Value in Tree: ", bst.smallestValue(root))    # Smallest Value in Tree:  2
+print("Smallest Value in Tree: ", bst.smallestValue(root).data)    # Smallest Value in Tree:  2
 print("Largest Value in Tree: ", bst.largestValue(root))      # Largest Value in Tree:  30
+
+
+# # Orginal InOrder: 2 5 8 9 10 18 20 30  
+
+# root = bst.deleteNode(root, 10)   # Delete 2 child
+# print("\nInorder: ")              # Inorder: 
+# bst.InOrder(root)                 # 2 5 8 9 18 20 30  
+
+# root = bst.deleteNode(root, 18)   # Delete 0 Child
+# print("\nInorder: ")              #Inorder: 
+# bst.InOrder(root)                 # 2 5 8 9 10 20 30 
+
+# root = bst.deleteNode(root, 5)    # Delete 1 child
+# print("\nInorder: ")              #Inorder: 
+# bst.InOrder(root)                 # 2 8 9 10 18 20 30 
